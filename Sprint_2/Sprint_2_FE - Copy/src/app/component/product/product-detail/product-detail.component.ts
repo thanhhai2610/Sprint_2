5ee07918aaa8f0e38ad5b024bd9a70388705e17d
thanhhai2610 <thanhai707726@gmail.com>
2023-01-05 17:27:02 +0700

    day4

---
+++ Sprint_2/Sprint_2_FE - Copy/src/app/component/product/product-detail/product-detail.component.ts
@@ -0,0 +1,18 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+
+@Component({
+  selector: 'app-product-detail',
+  templateUrl: './product-detail.component.html',
+  styleUrls: ['./product-detail.component.css'],
+  // encapsulation: ViewEncapsulation.None
+})
+export class ProductDetailComponent implements OnInit {
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
